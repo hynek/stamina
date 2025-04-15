7fda4518ff5df0da4539db8163e7a3fa9bfff614
Peter Gessler <gesslerpd@users.noreply.github.com>
2025-04-15 23:54:06 +0000

    fix tests

--- tests/test_async.py
+++ tests/test_async.py
@@ -49,7 +49,7 @@ async def test_ok(attempts, timeout, duration):
     assert 42 == await C().f()
 
 
-@pytest.mark.parametrize("timeout", [None, 0, 1, dt.timedelta(days=1)])
+@pytest.mark.parametrize("timeout", [None, 1, dt.timedelta(days=1)])
 @pytest.mark.parametrize("duration", [0, dt.timedelta(days=0)])
 async def test_retries(duration, timeout, on):
     """
